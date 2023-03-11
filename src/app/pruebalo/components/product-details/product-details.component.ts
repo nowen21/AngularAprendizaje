da09fbcb85e842c8f1248129594ef15549a488bf
Ing. Jose Dumar Jimenez Ruiz <nowen21@gmail.com>
2023-03-11 12:04:03 -0500

    Se agrega método:addToCart que es el encargado de adicionar un producto al carro

--- src/app/pruebalo/components/product-details/product-details.component.ts
+++ src/app/pruebalo/components/product-details/product-details.component.ts
@@ -11,13 +11,22 @@ import { Product, products } from '../products';
 })
 export class ProductDetailsComponent implements OnInit {
   product: Product | undefined;
-  constructor(private route: ActivatedRoute,private cartService: CartService) { }
+  constructor(private route: ActivatedRoute, private cartService: CartService) { }
   ngOnInit(): void {
     // Primero obtiene el id del producto de la ruta actual.
-  const routeParams = this.route.snapshot.paramMap;
-  const productIdFromRoute = Number(routeParams.get('productId'));
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
 
-  // Encuentra el producto que corresponde con el id proporcionado en la ruta.
-  this.product = products.find(product => product.id === productIdFromRoute);
+    // Encuentra el producto que corresponde con el id proporcionado en la ruta.
+    this.product = products.find(product => product.id === productIdFromRoute);
+  }
+
+  /**
+   * Agregar el producto al carro
+   * @param product 
+   */
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+    window.alert('Su producto ha sido agregado al carro!');
   }
 }
