8de1552274b152aca8508dbf58ca7dd69ea8498c
Ing. Jose Dumar Jimenez Ruiz <nowen21@gmail.com>
2023-03-11 11:47:34 -0500

    Se crea variable items y se importan los productos

--- src/app/pruebalo/servicios/cart.service.ts
+++ src/app/pruebalo/servicios/cart.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
+import { Product } from '../components/products';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
-
+  items: Product[] = [];
   constructor() { }
 }
