611d23433d7ee964d51180369a16004f435dc2c1
Ing. Jose Dumar Jimenez Ruiz <nowen21@gmail.com>
2023-03-11 12:48:38 -0500

    Se importa el servicio: CartService

--- src/app/pruebalo/components/cart/cart.component.ts
+++ src/app/pruebalo/components/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CartService } from '../../servicios/cart.service';
 
 @Component({
   selector: 'app-cart',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent {
-
+ 
 }
