47ff2e4ea4971b1279ba51c94edc70bdc16b8b70
Ing. Jose Dumar Jimenez Ruiz <nowen21@gmail.com>
2023-03-11 11:51:50 -0500

    Se adicionan los métodos addToCart, getItems y clearCart

--- src/app/pruebalo/servicios/cart.service.ts
+++ src/app/pruebalo/servicios/cart.service.ts
@@ -7,4 +7,30 @@ import { Product } from '../components/products';
 export class CartService {
   items: Product[] = [];
   constructor() { }
+  
+  /**
+   * Agragar un producto
+   * @param product 
+   */
+  addToCart(product: Product) {
+    this.items.push(product);
+  }
+
+  /**
+   * Obtener los productos
+   * @returns 
+   */
+  getItems() {
+    return this.items;
+  }
+
+  /**
+   * Limpiar los productos
+   * @returns 
+   */
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+
 }
